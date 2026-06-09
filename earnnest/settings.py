b59255ce7adfba7f4fd4c03855953db9e72ba50d
waabiud <marylyne179@gmail.com>
2026-06-09 06:14:07 +0300

    Added api app with REST endpoints

--- earnnest/settings.py
+++ earnnest/settings.py
@@ -275,9 +275,23 @@ REST_FRAMEWORK = {
 # CORS
 CORS_ALLOWED_ORIGINS = [
     'http://localhost:3000',
-    'http://10.0.2.2:8000',  # Android emulator
+    'http://127.0.0.1:3000',
+    'https://earnnest.onrender.com',
 ]
-CORS_ALLOW_CREDENTIALS = True
+
+CORS_ALLOW_ALL_ORIGINS = True  #
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 # JWT
 from datetime import timedelta
